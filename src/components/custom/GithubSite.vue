cea0519492f1047aabfcda15376e072ed781157f
HaoZhou3 <132916880+HaoZhou3@users.noreply.github.com>
2023-05-23 23:34:14 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <span>welcome to</span>
+    <a href="https://shop.testzh8.top" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
